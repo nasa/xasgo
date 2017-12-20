bbcdb9b51ced0aacebdb386688022f0a6ad4e837
Ruggles <truggles@ndc.nasa.gov>
2017-12-20 11:46:58 -0500

    Fixed CalcF

--- RunCalcFScriptOdd.jl
+++ RunCalcFScriptOdd.jl
@@ -7,12 +7,16 @@ function CalcF(ROIs, ROIsize, DefI, PD, RefI, PR)
 
 m = minimum(size(DefI))  # smallest dimension of image
 #qs = zeros(size(ROIs))
-ROIsize_px = 2*convert(Int64,m*ROIsize/200) #ROI forced to be even and square
+ROIsize_px = 2*round(Int64,m*ROIsize/200) #ROI forced to be even and square
 ccfilt = ccfilter(2,50, [ROIsize_px,ROIsize_px], 13)
 windowfunc = ccwindow(ROIsize_px)
 qerror = zeros(2,size(ROIs)[1])
 qerrorDIC = zeros(2,size(ROIs)[1])
 q_guess = []
+qs = zeros(2,size(ROIs)[1])
+q2s = zeros(2,size(ROIs)[1])
+rs = zeros(3,size(ROIs)[1])
+pppsum = zeros(6)
 for i in 1:size(ROIs)[1]
   println(i)
   ROI_px = m*ROIs[i,:]
@@ -25,18 +29,29 @@ for i in 1:size(ROIs)[1]
 
   thisq = MeasureShiftClassic_w_Shift(DefI, RefI, ROI_px, shiftROI_px, ROIsize_px, windowfunc, ccfilt)
 
-  if i == 1
-     q_guess = [trueq[2], trueq[1]]
-  end
-  testq = RunIcgn(DefI, RefI, (round.(ROI_px) - [0.5;0.5]), q_guess, ROIsize_px)
-  q_guess = testq
+  ppp = RunIcgn(DefI, RefI, (round.(ROI_px) - [0.5;0.5]), [trueq[2],trueq[1]], ROIsize_px)
+  display(ppp)
+  testq = ppp[1:2]
+
+  pppsum += ppp
 
   #println(trueq, thisq, testq)
   #println(" ")
   qerror[:,i] = thisq - [trueq[2], trueq[1]]
   qerrorDIC[:,i] = testq - [trueq[2], trueq[1]]
 
+  qstar = reverse_shift(thisq,ROI_px,PD,PR, m) #reverse_shift, shift_estimate_simple and the r calc below share a lot of calcs
+
+  qstarDIC = reverse_shift(testq,ROI_px,PD,PR, m)
+
+  qs[:,i] = [qstar[2], qstar[1]]/m
+  q2s[:,i] = [qstarDIC[2], qstarDIC[1]]/m
+  Qvp=[-1 0 0; 0 -1 0; 0 0 1]
+  rs[:,i] = (Qvp*(round.([ROI_px[2]; ROI_px[1]; 0]) - [0.5; 0.5; 0.]) + m*[PR[1]; 1-PR[2]; -PR[3]])/m
+
 end
+
+println(pppsum/size(ROIs)[1])
 #display(scatter(qerror[1,:],qerror[2,:]))
 println("mean X error: ", mean(qerror[1,:]))
 println("mean Y error: ", mean(qerror[2,:]))
@@ -46,10 +61,55 @@ println("DIC mean X error: ", mean(qerrorDIC[1,:]))
 println("DIC mean Y error: ", mean(qerrorDIC[2,:]))
 println("DIC std X error: ", std(qerrorDIC[1,:]))
 println("DIC std Y error: ", std(qerrorDIC[2,:]))
+
+β = beta_calc_Ruggles(qs,rs)
+
+display(β)
+
+β2 = beta_calc_Ruggles(q2s,rs)
+
+display(β2)
+
+display(β2./β)
+
+display(norm(β))
+display(norm(β2))
+
+display(180*acos((trace(β)+2)/2)/pi)
+display(180*acos((trace(β2)+2)/2)/pi)
+
+#display(det((β+eye(3))*(β'+eye(3))))
+#display(det((β2+eye(3))*(β2'+eye(3))))
+
+return β2
+
+end
+
+function beta_calc_Ruggles(qs,rs)
+  r1 = rs[1,:]
+  r2 = rs[2,:]
+  r3 = rs[3,:] #r3 is just a constant times a vector of ones...that's a wee bit silly
+  q1 = qs[1,:]
+  q2 = qs[2,:]
+  zerovec = zeros(size(r1))
+
+  A1 = [r1.*r3 r2.*r3 r3.*r3 zerovec zerovec zerovec -(r1.*r1 + q1.*r1) -(r1.*r2 + q1.*r2) -(r1.*r3 + q1.*r3)]
+  A2 = [zerovec zerovec zerovec r1.*r3 r2.*r3 r3.*r3 -(r2.*r1 + q2.*r1) -(r2.*r2 + q2.*r2) -(r2.*r3 + q2.*r3)]
+  b1 = q1.*r3;
+  b2 = q2.*r3;
+
+  b3 = 0
+  A3 = [1 0 0 0 1 0 0 0 1]
+
+  A = [A1;A2;A3]
+  b = [b1;b2;b3]
+
+  X = A\b
+  β = [X[1] X[2] X[3];X[4] X[5] X[6];X[7] X[8] X[9]]
 end
 
 function MeasureShiftClassic_w_Shift(DefI, RefI, ROI_px, shiftROI_px, ROIsize_px, windowfunc, ccfilt)
-  
+
   rrange=round(Int,ROI_px[1]-ROIsize_px/2):round(Int,ROI_px[1]-ROIsize_px/2)+ROIsize_px-1
   crange=round(Int,ROI_px[2]-ROIsize_px/2):round(Int,ROI_px[2]-ROIsize_px/2)+ROIsize_px-1
 
@@ -74,6 +134,16 @@ function shift_estimate_simple(ROI_px, PD, PR, m)
   δP = m*[-δP[1];δP[2];δP[3]]
   shiftROI3D = Qvp'*(-m*[PR[1];1-PR[2];-PR[3]] + δP + r*PD[3]/PR[3])
   shiftROI_px = [shiftROI3D[1];shiftROI3D[2]]
+end
+
+function reverse_shift(thisq,ROI_px,PD,PR, m)
+  Qvp=[-1 0 0; 0 -1 0; 0 0 1]
+  r = Qvp*(round.([ROI_px[2]; ROI_px[1]; 0]) - [0.5; 0.5; 0.]) + m*[PR[1]; 1-PR[2]; -PR[3]]
+  δP = PD - PR
+  δP = m*[-δP[1];δP[2];δP[3]]
+  ddratio = PR[3]/PD[3]
+  ph = (-[thisq[2], thisq[1]]-δP[1:2])*ddratio - r[1:2]*(1-ddratio)
+  qstar = [ph[2],ph[1]]
 end
 
 function findpeak(A)
@@ -147,23 +217,35 @@ function ccwindow(L)
   return windowfunc
 end
 
+function squareimage(I)
+  mn = size(I)
+  buffer = round(Int64,(mn[2] - mn[1])/2)
+  I_square = I[:,buffer+1:buffer+mn[1]]
+end
 
-RefI = load("ZeroCamElevation_x0y0.png")
+
+#RefI = load("ZeroCamElevation_x0y0.png")
+RefI = load("D:\\XASGO\\al_ebsd\\set1\\ebsd_0.png")
+RefI = squareimage(RefI)
 #display(heatmap(RefI))
 RefI = convert(Array{ColorTypes.Gray{FixedPointNumbers.Normed{UInt8,8}},2},RefI)
 RefI = convert(Array{Float64},RefI)
 RefI = filterimage(RefI,9,90,25)
-PR = [.5;.5;.7]
+#PR = [.5;.5;.7]
+PR = [.5;.5;.625]
 
-DefI = load("ZeroCamElevation_x500y500.png")
+#DefI = load("ZeroCamElevation_x500y500.png")
+DefI = load("D:\\XASGO\\al_ebsd\\set1\\ebsd_1.png")
+DefI = squareimage(DefI)
 DefI = convert(Array{ColorTypes.Gray{FixedPointNumbers.Normed{UInt8,8}},2},DefI)
 DefI = convert(Array{Float64},DefI)
 DefI = filterimage(DefI,9,90,25)
-PD = [.48046875;.518353371499725;.706680080924329] #x500y500
+#PD = [.48046875;.518353371499725;.706680080924329] #x500y500
 #PD = [.5;.51835337;.70668008] #x0y500
 #PD = [0.498046875000000;0.501835337149973;0.700668008092433]#x50y50
+PD = [.5;.5;.625]
 
-N = 24
+N = 48
 
 θs = 2*pi*collect(0:N-2)/(N-1)
 
@@ -177,6 +259,6 @@ for i=2:N
 end
 
 
-CalcF(ROIs, ROIsize, DefI, PD, RefI, PR)
+F = CalcF(ROIs, ROIsize, DefI, PD, RefI, PR)
 
 println("Done")
